c4eb76f38cb7f87400cb708730fcbabf4365cb50
Sunayana <sunayana.sarangi@gmail.com>
2024-06-12 13:14:19 +0200

    adjusted styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/form";
 
 export default function Home() {
   return (
-    <section className="bg-gradient-to-r from-purple-500 to-blue-300 py-24">
+    <section className="bg-gradient-to-r from-blue-700 to-purple-300 py-24">
       <h1 className="text-xl font-medium capitalize text-center sm:text-3xl md:text-3xl">
         Multistep React Hook Form
       </h1>
